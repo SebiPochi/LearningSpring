f63e7ebfc25372d91f05f7b7763a28275ed390e1
sebipochi <sebipochi@gmail.com>
2023-08-09 16:17:55 -0300

    init

---
+++ src/main/java/com/platzi/tutorial/PlatziTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziTutorialApplication.class, args);
+	}
+
+}
